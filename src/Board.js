28378a0488a06f9049fdc1c1bcf69498effe92df
Hack Reactor Student <student@hackreactor.com>
2017-10-11 15:16:30 -0500

    Test passing on first two

--- src/Board.js
+++ src/Board.js
@@ -62,7 +62,7 @@
     },
 
 
-/*
+    /*
          _             _     _
      ___| |_ __ _ _ __| |_  | |__   ___ _ __ ___ _
     / __| __/ _` | '__| __| | '_ \ / _ \ '__/ _ (_)
@@ -79,12 +79,26 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      var row = this.get(rowIndex);
+      var already1 = false;
+      for (var i = 0; i < row.length; i++) {
+        if (already1 && row[i] === 1) {
+          return true;
+        } else if (row[i] === 1) {
+          already1 = true;
+        }
+      }
+      return false;
     },
 
-    // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      var n = this.get('n');
+      for (var i = 0; i < n; i++) {
+        if (this.hasRowConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
